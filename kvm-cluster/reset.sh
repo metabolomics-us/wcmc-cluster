089863893a6666dc32fccc7488b36518b4113a98
berlinguyinca <berlinguyinca@gmail.com>
2019-08-13 00:47:40 +0000

    fixed parted partiotion generation

--- kvm-cluster/reset.sh
+++ kvm-cluster/reset.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i wcmc.inventory --become --ask-become-pass kvm_cluster.yml --tags drop_kvm,destroy_swarm
+ansible-playbook -i wcmc.inventory --become --ask-become-pass kvm_cluster.yml --tags destroy_swarm,drop_kvm
